3f8f9e45b83648de336789e8742feaf6066132b7
TanveerAhmadRaj <ta78296@gmail.com>
2024-05-20 16:21:17 +0500

    "enums added

---
+++ src/enums/index.ts
@@ -0,0 +1,18 @@
+
+
+
+export enum Gender {
+    MALE = "male",
+    FEMALE = "female",
+    OTHERS = "others"
+};
+
+export enum Rolls {
+    SUPER_ADMIN = "super admin",
+    ADMIN = "admin",
+    FLOOR_INCHARGE = "floor inchage"
+};
+
+export enum FacultyStatus {
+    
+};
